c19adcc279ad5eece14d4100b4a7e64ef3d6761e
13836005450 <597985642@qq.com>
2019-09-01 22:19:05 +0800

    新增分配账号页面

--- src/views/admin/AdminIndex.vue
+++ src/views/admin/AdminIndex.vue
@@ -1,68 +1,58 @@
 <template>
-    <div>
-        <Menu mode="horizontal" :theme="theme1" active-name="addUser" @on-select="select">
-            <MenuItem name="addUser">
-                <Icon type="ios-paper"/>
-                分配账号
-            </MenuItem>
-            <MenuItem name="mangerUser">
-                <Icon type="ios-paper"/>
-                查看已分配账号
-            </MenuItem>
-            <MenuItem name="submitStatus">
-                <Icon type="ios-paper"/>
-                查看账户提交状态
-            </MenuItem>
-            <MenuItem name="adminChangePassword">
-                <Icon type="ios-paper"/>
-                修改密码
-            </MenuItem>
-            <div class="user">
-                <div class="">欢迎登录，{{companyName}}</div>
-                <div class="logout">
-                    <span @click="logout">退出登录</span>
-                </div>
-            </div>
-        </Menu>
-        <router-view></router-view>
-    </div>
+  <div>
+    <Menu mode="horizontal" :theme="theme1" active-name="addUser" @on-select="select">
+      <MenuItem name="addUser">
+        <Icon type="ios-paper" />分配账号
+      </MenuItem>
+      <MenuItem name="submitStatus">
+        <Icon type="ios-paper" />查看下级部门
+      </MenuItem>
+      <MenuItem name="adminChangePassword">
+        <Icon type="ios-paper" />修改我的密码
+      </MenuItem>
+      <div class="user">
+        <div class>欢迎登录，{{companyName}}</div>
+        <div class="logout">
+          <span @click="logout">退出登录</span>
+        </div>
+      </div>
+    </Menu>
+    <router-view></router-view>
+  </div>
 </template>
 <style scoped>
-    .user {
-        display: flex;
-        position: absolute;
-        right: 30px;
-        top: 0;
-    }
-    .logout {
-        color: #8CD1FF;
-        cursor: pointer;
-        margin: 0 20px;
-    }
-
-
-
+.user {
+  display: flex;
+  position: absolute;
+  right: 30px;
+  top: 0;
+}
+.logout {
+  color: #8cd1ff;
+  cursor: pointer;
+  margin: 0 20px;
+}
 </style>
 
 <script>
-    export default {
-        data(){
-            return {
-                theme1: 'light',
-                companyName: '哈尔滨理工大学'
-            }
-        },
-        methods: {
-            select(name){
-                console.log(name)
-                this.$router.push(name);
-            },
-            logout(){
-                this.$router.push('/')
-            }
-        },
-        created(){
-            this.$router.push('/admin/addUser')
-        },
+export default {
+  data() {
+    return {
+      theme1: "light",
+      companyName: "哈尔滨理工大学"
+    };
+  },
+  methods: {
+    select(name) {
+      console.log(name);
+      this.$router.push(name);
+    },
+    logout() {
+      this.$router.push("/");
     }
+  },
+  created() {
+    this.$router.push("/admin/addUser");
+  }
+};
 </script>

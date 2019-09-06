4e9c42877e89375d4f0beedb63cd406b2a434983
Qccccc's laptop <896853205@qq.com>
2019-09-06 15:12:20 +0800

    添加了一些模块

--- src/service.config.js
+++ src/service.config.js
@@ -2,23 +2,36 @@
 const SERVERURL = "http://localhost:3000/";
 // const SERVERURL = "http://localhost:1234/";
 
-const URL = {
-    checkLogin: SERVERURL + 'checkLogin',
-    saveUser: SERVERURL + 'saveUser',
-    getCompanyInfo: SERVERURL + 'getCompanyInfo',
-    test: SERVERURL + 'test',
-    post: SERVERURL + 'test2',
-    save: SERVERURL + 'companyFormSave',
-	reg: SERVERURL + 'register',
-	login: SERVERURL + 'login',
-	getForm: SERVERURL + 'companyFormGet',
-	submit: SERVERURL + 'companyFormSubmit',
-	changePass: SERVERURL + 'changePassWord',
-    createTable: SERVERURL + 'createTable',
-    download: SERVERURL + 'download',
-    adminLogin: SERVERURL + 'adminLogin',
-    generateExcel: SERVERURL + 'generateExcel',
+const PORT = {
+  admin: 'admin'
+}
 
+const URL = {
+  // 问卷填报路由
+  checkLogin: SERVERURL + 'checkLogin',
+  saveUser: SERVERURL + 'saveUser',
+  getCompanyInfo: SERVERURL + 'getCompanyInfo',
+  test: SERVERURL + 'test',
+  post: SERVERURL + 'test2',
+  save: SERVERURL + 'companyFormSave',
+  reg: SERVERURL + 'register',
+  login: SERVERURL + 'login',
+  getForm: SERVERURL + 'companyFormGet',
+  submit: SERVERURL + 'companyFormSubmit',
+  changePass: SERVERURL + 'changePassWord',
+  createTable: SERVERURL + 'createTable',
+  download: SERVERURL + 'download',
+  adminLogin: SERVERURL + 'adminLogin',
+  generateExcel: SERVERURL + 'generateExcel',
+  
+  // 权限分配路由
+  adminLogin: `${SERVERURL}${PORT.admin}/login`,
+  adminRegister: `${SERVERURL}${PORT.admin}/register`,
+  manageInfo: `${SERVERURL}${PORT.admin}/manageInfo`,
+  submitManage: `${SERVERURL}${PORT.admin}/submitManage`,
+  alterPassword: `${SERVERURL}${PORT.admin}/alterPassword`
 };
+
+
 
 export default URL;
--- src/store/admin/index.js
+++ src/store/admin/index.js
@@ -1,8 +1,20 @@
 export default {
   // 权限管理的数据存在这里
-  state: {},
+  state: {
+    adminUserId: '',
+    userCode: ''
+  },
   // 操作vuex的函数放在这里
-  mutations: {},
+  mutations: {
+    // 设置管理权限模块的userid
+    adminUserId (state, adminUserId) {
+      state.adminUserId = adminUserId;
+    },
+    // 设置userCode
+    setUserCode (state, userCode) {
+      state.userCode = userCode;
+    }
+  },
   // 获取vuex中的数据放在这里
   getters: {}
 }
